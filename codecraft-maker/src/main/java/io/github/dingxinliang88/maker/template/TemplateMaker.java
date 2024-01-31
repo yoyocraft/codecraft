e428c58b7664c412287ce81a510ab9997429271b
CodeJuzi <youyichannel@163.com>
2024-01-31 15:41:54 +0800

    feat(template_maker): build basic process

---
+++ codecraft-maker/src/main/java/io/github/dingxinliang88/maker/template/TemplateMaker.java
@@ -0,0 +1,85 @@
+package io.github.dingxinliang88.maker.template;
+
+import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.util.StrUtil;
+import cn.hutool.json.JSONUtil;
+import io.github.dingxinliang88.maker.meta.Meta;
+import io.github.dingxinliang88.maker.meta.enums.FileGenerateTypeEnum;
+import io.github.dingxinliang88.maker.meta.enums.FileTypeEnum;
+import io.github.dingxinliang88.maker.meta.enums.ModelTypeEnum;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 模板制作
+ *
+ * @author <a href="https://github.com/dingxinliang88">youyi</a>
+ */
+public class TemplateMaker {
+
+    public static void main(String[] args) {
+        // 1. 输入信息
+        // 1.1 项目基本信息
+        String name = "acm-template-generator";
+        String description = "ACM 示例模板生成器";
+
+        // 1.2 文件信息
+        String projectPath = System.getProperty("user.dir");
+        String sourceRootPath =
+                new File(projectPath).getParent() + File.separator + "sample/acm-template";
+        // 兼容 win
+        sourceRootPath = sourceRootPath.replaceAll("\\\\", "/");
+
+        String fileInputPath = "src/io/github/dingxinliang88/acm/MainTemplate.java";
+        String fileOutputPath = fileInputPath + ".ftl";
+
+        // 1.3 模型参数信息
+        Meta.ModelConfig.ModelInfo modelInfo = new Meta.ModelConfig.ModelInfo();
+        modelInfo.setFieldName("outputTest");
+        modelInfo.setType(ModelTypeEnum.STRING.getValue());
+        modelInfo.setDefaultValue("sum = ");
+
+        // 2. 使用字符串替换算法，生成 ftl 模板文件
+        String fileInputAbsolutePath = sourceRootPath + File.separator + fileInputPath;
+        String fileContent = FileUtil.readUtf8String(fileInputAbsolutePath);
+        String replacement = String.format("${%s}", modelInfo.getFieldName());
+        String newFileContent = StrUtil.replace(fileContent, "Sum: ", replacement);
+
+        // 输出模板文件
+        String fileOutputAbsolutePath = sourceRootPath + File.separator + fileOutputPath;
+        FileUtil.writeUtf8String(newFileContent, fileOutputAbsolutePath);
+
+        // 3. 生成 meta.json 文件
+        String metaOutputPath = sourceRootPath + File.separator + "meta.json";
+
+        // 3.1 构造配置参数
+        Meta meta = new Meta();
+        meta.setName(name);
+        meta.setDescription(description);
+
+        Meta.FileConfig fileConfig = new Meta.FileConfig();
+        meta.setFileConfig(fileConfig);
+        fileConfig.setSourceRootPath(sourceRootPath);
+        List<Meta.FileConfig.FileInfo> fileInfoList = new ArrayList<>();
+        fileConfig.setFiles(fileInfoList);
+
+        Meta.FileConfig.FileInfo fileInfo = new Meta.FileConfig.FileInfo();
+        fileInfo.setInputPath(fileInputPath);
+        fileInfo.setOutputPath(fileOutputPath);
+        fileInfo.setType(FileTypeEnum.FILE.getValue());
+        fileInfo.setGenerateType(FileGenerateTypeEnum.DYNAMIC.getValue());
+        fileInfoList.add(fileInfo);
+
+        Meta.ModelConfig modelConfig = new Meta.ModelConfig();
+        meta.setModelConfig(modelConfig);
+        List<Meta.ModelConfig.ModelInfo> modelInfoList = new ArrayList<>();
+        modelConfig.setModels(modelInfoList);
+        modelInfoList.add(modelInfo);
+
+        // 3.2 输出 meta.json 元信息文件
+        FileUtil.writeUtf8String(JSONUtil.toJsonPrettyStr(meta), metaOutputPath);
+
+    }
+
+}
