7f3953a4dc33e093495f349b39d880be87161a4a
CodeJuzi <youyichannel@163.com>
2024-01-31 15:58:34 +0800

    feat(template_maker): isolate work space and upgrade hutool version(5.8.25)

--- codecraft-maker/src/main/java/io/github/dingxinliang88/maker/template/TemplateMaker.java
+++ codecraft-maker/src/main/java/io/github/dingxinliang88/maker/template/TemplateMaker.java
@@ -1,6 +1,7 @@
 package io.github.dingxinliang88.maker.template;
 
 import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.util.IdUtil;
 import cn.hutool.core.util.StrUtil;
 import cn.hutool.json.JSONUtil;
 import io.github.dingxinliang88.maker.meta.Meta;
@@ -8,6 +9,7 @@ import io.github.dingxinliang88.maker.meta.enums.FileGenerateTypeEnum;
 import io.github.dingxinliang88.maker.meta.enums.FileTypeEnum;
 import io.github.dingxinliang88.maker.meta.enums.ModelTypeEnum;
 import java.io.File;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -19,15 +21,28 @@ import java.util.List;
 public class TemplateMaker {
 
     public static void main(String[] args) {
+        // 0. 工作空间隔离
+        // 0.1 指定原始项目路径
+        String projectPath = System.getProperty("user.dir");
+        String originProjectPath =
+                new File(projectPath).getParent() + File.separator + "sample/acm-template";
+        // 0.2 复制目录
+        long id = IdUtil.getSnowflakeNextId();
+        String tmpDirPath = projectPath + File.separator + ".tmp";
+        String templatePath = tmpDirPath + File.separator + id;
+        if (!FileUtil.exist(templatePath)) {
+            FileUtil.mkdir(templatePath);
+        }
+        FileUtil.copy(originProjectPath, templatePath, true);
+
         // 1. 输入信息
         // 1.1 项目基本信息
         String name = "acm-template-generator";
         String description = "ACM 示例模板生成器";
 
         // 1.2 文件信息
-        String projectPath = System.getProperty("user.dir");
-        String sourceRootPath =
-                new File(projectPath).getParent() + File.separator + "sample/acm-template";
+        String sourceRootPath = templatePath + File.separator +
+                FileUtil.getLastPathEle(Paths.get(originProjectPath)).toString();
         // 兼容 win
         sourceRootPath = sourceRootPath.replaceAll("\\\\", "/");
 
@@ -50,8 +65,8 @@ public class TemplateMaker {
         String fileOutputAbsolutePath = sourceRootPath + File.separator + fileOutputPath;
         FileUtil.writeUtf8String(newFileContent, fileOutputAbsolutePath);
 
-        // 3. 生成 meta.json 文件
-        String metaOutputPath = sourceRootPath + File.separator + "meta.json";
+        // 3. 生成 meta.json 文件，和 template 同级
+        String metaOutputPath = templatePath + File.separator + "meta.json";
 
         // 3.1 构造配置参数
         Meta meta = new Meta();
