e71f11edaaf1737d451928895ddae9564eb853b8
游艺(youyi) <youyichannel@163.com>
2024-02-14 15:18:20 +0800

    refactor(maker): use enum to implement singleton meta info

--- codecraft-maker/src/main/java/io/github/dingxinliang88/maker/meta/MetaManager.java
+++ codecraft-maker/src/main/java/io/github/dingxinliang88/maker/meta/MetaManager.java
@@ -2,6 +2,7 @@ package io.github.dingxinliang88.maker.meta;
 
 import cn.hutool.core.io.resource.ResourceUtil;
 import cn.hutool.json.JSONUtil;
+import lombok.Getter;
 
 /**
  * 元信息管理器
@@ -10,30 +11,30 @@ import cn.hutool.json.JSONUtil;
  */
 public class MetaManager {
 
-
-    private static volatile Meta meta;
-
     private MetaManager() {
     }
 
     public static Meta getMeta() {
-        if (meta == null) {
-            synchronized (MetaManager.class) {
-                if (meta == null) {
-                    meta = initMeta();
-                }
-            }
+        return MetaHolder.INSTANCE.getMeta();
+    }
+
+    @Getter
+    private enum MetaHolder {
+        INSTANCE;
+
+        private final Meta meta;
+
+        MetaHolder() {
+            meta = initMeta();
         }
-        return meta;
-    }
 
-    private static Meta initMeta() {
-        String metaJson = ResourceUtil.readUtf8Str("meta.json");
-//        String metaJson = ResourceUtil.readUtf8Str("springboot-init-meta.json");
-        Meta newMeta = JSONUtil.toBean(metaJson, Meta.class);
-        MetaValidator.doValidateAndFill(newMeta);
-        return newMeta;
+        private Meta initMeta() {
+            String metaJson = ResourceUtil.readUtf8Str("meta.json");
+            // String metaJson = ResourceUtil.readUtf8Str("springboot-init-meta.json");
+            Meta newMeta = JSONUtil.toBean(metaJson, Meta.class);
+            MetaValidator.doValidateAndFill(newMeta);
+            return newMeta;
+        }
     }
-
 
 }
